a52bf880d977fadd794083c8a1075c6dac0173c9
Robson <robson14br@outlook.com>
2024-07-10 23:40:16 -0300

    feat trip-details

--- frontend-react/src/pages/create-trip/invite-guests-modal.tsx
+++ frontend-react/src/pages/create-trip/invite-guests-modal.tsx
@@ -1,5 +1,6 @@
 import { AtSign, Plus, X } from "lucide-react"
 import { FormEvent } from "react"
+import { Button } from "../../components/button"
 
 interface InviteGuestsModalProps {
     closeGuestsModal: () => void
@@ -58,10 +59,10 @@ return (
           className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
           </div>
 
-          <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-           Convidar
-           <Plus className="size-5" />
-         </button>
+          <Button type="submit">
+            Convidar
+            <Plus className="size-5" />
+          </Button>
         </form>
   </div>
 </div>
